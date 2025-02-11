ca509ab9c564d10e5fe0a28a1aaa8196371c5231
saw44169 <92521816+saw44169@users.noreply.github.com>
2025-02-11 15:49:05 +0900

    build: AssenmlyInfoを更新

--- SteamP2PInfo/Properties/AssemblyInfo.cs
+++ SteamP2PInfo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SteamP2PInfo")]
+[assembly: AssemblyTitle("SteamP2PInfo-rubiconian")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SteamP2PInfo")]
+[assembly: AssemblyProduct("SteamP2PInfo-rubiconian")]
 [assembly: AssemblyCopyright("Copyright © fork by saw44169, original by tremwil 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
